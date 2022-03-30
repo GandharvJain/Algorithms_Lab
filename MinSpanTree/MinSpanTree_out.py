b428cdedee3e76bfdceb1c2355c07fc2e5cfe6b5
Gandharv Jain <78318488+GandharvJain@users.noreply.github.com>
2022-03-30 20:05:05 +0530

    Add KruskalSecondBestMST.cpp

--- MinSpanTree/MinSpanTree_out.py
+++ MinSpanTree/MinSpanTree_out.py
@@ -31,3 +31,22 @@ plt.savefig('mst_in.png', bbox_inches='tight')
 nx.draw_networkx_edges(G, pos, edgelist=edges, edge_color='white', arrows=False, width=1.3)
 nx.draw_networkx_edges(G, pos, edgelist=mst_edges, edge_color='r', arrows=False, width=2)
 plt.savefig('mst_out.png', bbox_inches='tight')
+plt.clf()
+
+
+try:
+    with open('out1.txt', 'r') as f:
+        lines = f.read().split("\n")
+        mst1_edges = [line.split(" ") for line in lines[1:] if line]
+
+    mst1_edges = [(int(c[0]), int(c[1]), int(c[2])) for c in mst1_edges]
+    nx.draw_networkx_nodes(G, pos, node_color = values, node_size = 300)
+    nx.draw_networkx_labels(G, pos)
+    nx.draw_networkx_edge_labels(G,pos,edge_labels=labels)
+    nx.draw_networkx_edges(G, pos, edgelist=edges, arrows=False)
+    nx.draw_networkx_edges(G, pos, edgelist=edges, edge_color='white', arrows=False, width=1.3)
+    nx.draw_networkx_edges(G, pos, edgelist=mst1_edges, edge_color='r', arrows=False, width=2)
+    plt.savefig('mst1_out.png', bbox_inches='tight')
+    plt.clf()
+except Exception as e:
+    raise e
